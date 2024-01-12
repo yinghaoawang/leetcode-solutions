f4f97bd1e078fb926750a72ffee376b3e08c5e31
Yinghao Wang <yinghao.a.wang@gmail.com>
2024-01-11 20:01:49 -0800

    add merge two binary trees

---
+++ 617-merge-two-binary-trees.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(root1, root2) {
+    if (root1 == null && root2 == null) return null;
+    const res = new TreeNode((root1 != null ? root1.val : 0) + (root2 != null ? root2.val : 0));
+    console.log(root1?.val, root2?.val, res.val);
+    res.left = mergeTrees(root1?.left, root2?.left);
+    res.right = mergeTrees(root1?.right, root2?.right);
+    return res;
+};
