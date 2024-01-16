abc65fc685ae75e7d571224e787e70bfec679a2d
Yinghao Wang <yinghao.a.wang@gmail.com>
2024-01-15 20:51:28 -0800

    add bst to gst

---
+++ 1038-binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    // get sum, traverse inorder and subtract from the sum every time
+    const sum = getSum(root);
+
+    function getSum(node) {
+        if (node == null) return 0;
+        const lSum = getSum(node.left);
+        const rSum = getSum(node.right);
+        return lSum + rSum + node.val;
+    }
+
+    function traverse(node, value=26) {
+        if (node == null) return value;
+        const lVal = traverse(node.left, value);
+        
+        const origVal = node.val;
+        node.val = lVal;
+
+        const rVal = traverse(node.right, lVal - origVal)
+        return rVal;
+    }
+
+    traverse(root, sum);
+
+    return root;
+
+};
