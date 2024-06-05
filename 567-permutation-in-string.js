81c12ac020deef2bcc35d97ca91e1441f92869a4
Yinghao Alan Wang <yinghao.a.wang@gmail.com>
2024-06-05 13:46:07 -0700

    improved even more permutation in string

--- 567-permutation-in-string.js
+++ 567-permutation-in-string.js
@@ -4,8 +4,7 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-  // disclaimer: bottom 5% of all performance and memory :(
-  // i do an improved version after
+  // disclaimer: bottom 5% of all performance and memory :()
 
   // s1: a=1, b=1
 
@@ -54,34 +53,72 @@ var checkInclusion = function(s1, s2) {
 
   // return false;
 
-  // improved version: 20x faster
   // i want to try to get it by the size of s1 since i know that s1 is the key and s2 is the haystack
+  // if (s1.length > s2.length) return false;
+  // let l = 0, r = s1.length - 1, s1Map = {}, s2Map = {};
+  // for (const c of s1) s1Map[c] = s1Map[c] + 1 || 1;
+  // for (let i = 0; i < s1.length; i++) {
+  //     const c = s2[i];
+  //     s2Map[c] = s2Map[c] + 1 || 1;
+  // }
+  // while (r < s2.length) {
+  //     // console.log(l, r, s2Map);
+  //     if (mapEqual(s1Map, s2Map)) return true;
+  //     s2Map[s2[l]]--;
+  //     if (s2Map[s2[l]] === 0) delete s2Map[s2[l]];
+  //     l++, r++;
+  //     if (s2[r] >= s2.length) break;
+  //     s2Map[s2[r]] = s2Map[s2[r]] + 1 || 1;
+  // }
+
+
+  // function mapEqual(map1, map2) {
+  //     for (const key in map1) {
+  //         if (map2[key] != map1[key]) return false;
+  //     }
+  //     for (const key in map2) {
+  //         if (map2[key] != map1[key]) return false;
+  //     }
+  //     return true;
+  // }
+  // return false;
+
+  // improved improved version
   if (s1.length > s2.length) return false;
+  let matches = 0;
   let l = 0, r = s1.length - 1, s1Map = {}, s2Map = {};
   for (const c of s1) s1Map[c] = s1Map[c] + 1 || 1;
   for (let i = 0; i < s1.length; i++) {
       const c = s2[i];
       s2Map[c] = s2Map[c] + 1 || 1;
   }
+
+  for (let i = 0; i < 26; i++) {
+      let c = String.fromCharCode('a'.charCodeAt(0) + i);
+      s1Map[c] = s1Map[c] || 0;
+      s2Map[c] = s2Map[c] || 0;
+      matches += s1Map[c] === s2Map[c] ? 1 : 0;
+  }
+
   while (r < s2.length) {
-      // console.log(l, r, s2Map);
-      if (mapEqual(s1Map, s2Map)) return true;
-      s2Map[s2[l]]--;
-      if (s2Map[s2[l]] === 0) delete s2Map[s2[l]];
-      l++, r++;
-      if (s2[r] >= s2.length) break;
-      s2Map[s2[r]] = s2Map[s2[r]] + 1 || 1;
+      // console.log(l, r, s2Map, matches);
+      if (matches === 26) return true;
+
+      r++;
+      const rChar = s2[r];
+      s2Map[rChar] = s2Map[rChar] + 1 || 1;
+
+      if (s2Map[rChar] === s1Map[rChar]) matches++;
+      else if (s2Map[rChar] === s1Map[rChar]+1) matches--;
+
+      l++;
+      const lChar = s2[l-1];
+      s2Map[lChar]--;
+
+      if (s2Map[lChar] === (s1Map[lChar] || 0)) matches++;
+      else if (s2Map[lChar] === (s1Map[lChar] || 0)-1) matches--;
+
   }
 
-
-  function mapEqual(map1, map2) {
-      for (const key in map1) {
-          if (map2[key] != map1[key]) return false;
-      }
-      for (const key in map2) {
-          if (map2[key] != map1[key]) return false;
-      }
-      return true;
-  }
-  return false;
+  return matches === 26;
 };
